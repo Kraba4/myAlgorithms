80ebace63421fc1f495176fdd18e2eaed963a613
HONOR <a.krasnov2001@mail.ru>
2023-02-15 19:21:27 +0800

    basic avl tree logic done

--- myStructures.h
+++ myStructures.h
@@ -5,91 +5,170 @@
 #ifndef MYALGORITHMS_MYSTRUCTURES_H
 #define MYALGORITHMS_MYSTRUCTURES_H
 #include <algorithm>
+#include <list>
+
 namespace my{
     template<typename T>
     class TreeAVL{
         struct Node{
             Node* left = nullptr;
             Node* right = nullptr;
-            Node* parent;
             T value;
-            int diff = 0;
-            Node(Node* parent, T&& value) :parent(parent), value(std::move(value)){}
-
+            int h = 1;
+            Node(T&& value) : value(std::move(value)){
+            }
+            int calcDiff() {
+                int leftH = left == nullptr ? 0 : left->h;
+                int rightH = right == nullptr ? 0 : right->h;
+                return leftH - rightH;
+            }
+            void recalculateHeight() {
+                int leftH = left == nullptr ? 0 : left->h;
+                int rightH = right == nullptr ? 0 : right->h;
+                h = 1 + std::max(leftH, rightH);
+            }
         };
-        Node* root = nullptr;
-        void insert(Node* head, T&& value){
-            if(value == head->value){
-                return;
-            }
-            if(value < head->value){
-                if(head->left == nullptr){
-                    head->left = new Node(head, std::move(value));
-                }else {
-                    insert(head->left, std::move(value));
-                }
 
-            }else{
-                if(head->right == nullptr){
-                    head->right = new Node(head, std::move(value));
-                }else {
-                    insert(head->right, std::move(value));
-                }
-            }
+
+
+        Node* root = nullptr;
+
+        void rotateLeft(Node*& current){
+            Node* newCurrent = current->right;
+            current->right = newCurrent->left;
+            newCurrent->left = current;
+
+            current->recalculateHeight();
+            newCurrent->recalculateHeight();
+            current = newCurrent;
         }
-        bool contains(Node* head, const T& value){
-            if(head == nullptr){
-                return false;
-            }
-            if(value == head->value){
+        void rotateRight(Node*& current){
+            Node* newCurrent = current->left;
+            current->left = newCurrent->right;
+            newCurrent->right = current;
+
+            current->recalculateHeight();
+            newCurrent->recalculateHeight();
+            current = newCurrent;
+        }
+        bool balance(Node*& current) {
+            if(current== nullptr){
                 return true;
             }
-            if(value < head->value){
-                return contains(head->left, value);
+            int currentDiff = current->calcDiff();
+            int oldHeight = current->h;
+            if(currentDiff == 0) {
+                current->recalculateHeight();
+                return oldHeight != current->h;  // not needBalance
+            }
+            if(currentDiff == -1 || currentDiff == 1) {
+                current->recalculateHeight();
+                return oldHeight != current->h; //needBalance
+            }
+            if(currentDiff == -2) {
+                int rightDiff = current->right->calcDiff();
+                if (rightDiff == 0 || rightDiff == -1) {
+                    rotateLeft(current);
+                } else {
+                    rotateRight(current->right);
+                    rotateLeft(current);
+                }
+//                std::cout << "[" << current->calcDiff() << "]\n";
+//                return current->calcDiff() != 0;//needBalance
+                return oldHeight != current->h;
+            }
+            if(currentDiff == 2){
+                int leftDiff = current->left->calcDiff();
+                if(leftDiff == 0 || leftDiff == 1){
+                    rotateRight(current);
+                } else{
+                    rotateLeft(current->left);
+                    rotateRight(current);
+                }
+//                std::cout << "[" << current->calcDiff() << "]\n";
+//                return current->calcDiff() != 0; //needBalance
+                return oldHeight != current->h;
+            }
+            //error tree balance
+        }
+        bool insert(Node*& current, T&& value){
+            if(current == nullptr){
+                current = new Node(std::move(value));
+                return true; //needBalance
+            }
+            if(value == current->value){
+                return false; //needBalance
+            }
+
+            bool needBalance;
+            if(value < current->value){
+                needBalance = insert(current->left, std::move(value));
             }else{
-                return contains(head->right, value);
+                needBalance = insert(current->right, std::move(value));
+            }
+            if(needBalance) {
+                return balance(current);
+            }
+            return false; //needBalance
+        }
+        bool contains(Node* current, const T& value){
+            if(current == nullptr){
+                return false;
+            }
+            if(value == current->value){
+                return true;
+            }
+            if(value < current->value){
+                return contains(current->left, value);
+            }else{
+                return contains(current->right, value);
             }
         }
-        void erase(Node*& head,  const T& value){
-            if(head == nullptr){
-                return;
+        bool replaceAndDelete(Node*& removable, Node*& replacement){
+            if(replacement->left->left != nullptr){ //parent of replacement
+                bool needBalance = replaceAndDelete(removable, replacement->left);
+                if(needBalance) {
+                    return balance(replacement);
+                }
+                return false;
+            }else{
+                Node* trueReplacement = replacement->left;
+                replacement->left = trueReplacement->right;
+
+                trueReplacement->left = removable->left;
+                trueReplacement->right = removable->right;
+
+                delete removable;
+                removable = trueReplacement;
+                return balance(replacement);
             }
-            if(value == head->value){
-                Node* smallestRight = head->right;
-                if(smallestRight == nullptr){
-                    /*         H
-                     *        /
-                     *      [L]
-                     */
-                    if(head->left != nullptr) {
-                        head->left->parent = head->parent;
-                    }
-                    Node* left = head->left;
-
-                    delete head;
-                    head = left;
-                    return;
-                }
-                if(smallestRight->left == nullptr){
-                    /*
-                     *         H
-                     *       /  \
-                     *     [L]   R
-                     *             \
-                     *             [r]
-                     */
-                    smallestRight->parent = head->parent;
-                    smallestRight->left = head->left;
-                    if(smallestRight->left != nullptr){
-                        smallestRight->left->parent = smallestRight;
-                    }
-
-                    delete head;
-                    head = smallestRight;
-                    return;
-                }
-                /*
-                 *           H
+        }
+        bool eraseNode(Node*& current){
+            Node* smallestRight = current->right;
+            if(smallestRight == nullptr){
+                /*         H
+                 *        /
+                 *      [L]
+                 */
+                Node* left = current->left;
+                delete current;
+                current = left;
+                return true;
+            }
+            else if(smallestRight->left == nullptr){
+                /*         H
+                 *       /  \
+                 *     [L]   R
+                 *             \
+                 *             [r]
+                 */
+                smallestRight->left = current->left;
+                delete current;
+                current = smallestRight;
+                return true;
+            }
+            else {
+                /*           H
                  *         /  \
                  *        L    R
                  *           /
@@ -99,47 +178,100 @@ namespace my{
                  *           \
                  *           [r]
                  */
-                while (smallestRight->left != nullptr){
-                    smallestRight = smallestRight->left;
+                bool needBalance = replaceAndDelete(current, smallestRight);
+                current->right = smallestRight;
+                if(needBalance){
+                    return balance(current);
                 }
-                smallestRight->parent->left = smallestRight->right;
-                if(smallestRight->right!= nullptr){
-                    smallestRight->right->parent = smallestRight->parent;
-                }
-                smallestRight->parent = head->parent;
-                smallestRight->left = head->left;
-                if(smallestRight->left != nullptr) {
-                    smallestRight->left->parent = smallestRight;
-                }
-                smallestRight->right = head->right;
-                smallestRight->right->parent = smallestRight;
-
-                delete head;
-                head = smallestRight;
-                return;
-            }
-            if(value < head->value){
-                erase(head->left, value);
-            }else{
-                erase(head->right, value);
+                return false; //not needBalance
             }
         }
-        void print(Node* head){
-            if(head == nullptr){
+        bool erase(Node*& current,  const T& value){
+            if(current == nullptr){
+                return false;
+            }
+            bool needBalance;
+            if(value == current->value){
+                eraseNode(current);
+                balance(current);
+                return true;
+            }else if(value < current->value){
+                needBalance = erase(current->left, value);
+            }else{
+                needBalance = erase(current->right, value);
+            }
+
+            if(needBalance){
+                return balance(current);
+            }
+        }
+        void print(Node* current, int dep, char ch){
+            if(current == nullptr){
                 return;
             }
-            print(head->left);
-            std::cout << head->value << ' ';
-            print(head->right);
+            print(current->left, dep + 1, '/');
+            std::cout.width(dep * 5);
+            std::cout << ch << "(" << current->value << ',' <<current->calcDiff() << ") \n";
+            print(current->right, dep + 1, '\\');
         }
     public:
-        void insert(T value){
-            if(root == nullptr){
-                root = new Node(nullptr, std::move(value));
-                return;
+        class iterator{
+            std::list<Node*> parents;
+            Node* current;
+        public:
+            iterator(Node* node) : current(node){
+                while(current->left!= nullptr){
+                    parents.push_back(current);
+                    current = current->left;
+                }
             }
+            iterator& operator++(){
+                if(current->right != nullptr){
+                    parents.push_back(current);
+                    current = current->right;
+                    while(current->left != nullptr){
+                        parents.push_back(current);
+                        current = current->left;
+                    }
+                }else{
+                    while(parents.back()->value < current->value){
+                        parents.pop_back();
+                    }
+                    current = parents.back();
+                    parents.pop_back();
+                }
+                return *this;
+            }
+            iterator& operator--(){
+                if(current->left != nullptr){
+                    parents.push_back(current);
+                    current = current->left;
+                    while (current->right != nullptr){
+                        parents.push_back(current);
+                        current = current->right;
+                    }
+                }else{
+                    while(parents.back()->value > current->value){
+                        parents.pop_back();
+                    }
+                    current = parents.back();
+                    parents.pop_back();
+                }
+                return *this;
+            }
+            const T operator*(){
+                return current->value;
+            }
+        };
+        iterator begin(){
+            return iterator(root);
+        }
+        void insert(T value){
 //            std::cout << "insert "<< value <<std::endl;
-            insert(root, std::move(value));
+            bool needBalance = insert(root, std::move(value));
+            if(needBalance){
+                balance(root);
+            }
         }
         bool contains(const T& value){
             if(root == nullptr) {
@@ -148,12 +280,13 @@ namespace my{
             return contains(root, value);
         }
        void erase(T value){
-           erase(root, value);
+           bool needBalance = erase(root, value);
+           if(needBalance){
+               balance(root);
+           }
         }
         void print(){
-            std::cout << "{ ";
-            print(root);
-            std::cout << "}";
+            print(root, 0, '=');
         }
     };
 }
--- tree_test.cpp
+++ tree_test.cpp
@@ -4,25 +4,48 @@
 #include <iostream>
 #include "myStructures.h"
 #include <random>
+#include <set>
 #include "Utils.h"
 int main(){
+    std::set<int> c;
     my::TreeAVL<int> tree;
 //    tree.insert(1);
 //    tree.erase(1);
 //    tree.print();
-    std::vector a= utils::create_random_vector(1000, 1000, time(NULL));
+    std::vector a= utils::create_random_vector(20, 40, time(NULL));
     auto gen =  std::shuffle_order_engine<std::minstd_rand0, 256>(time(NULL));
     std::shuffle(a.begin(), a.end(), gen);
     for(auto x : a){
+        std::cout << x<< ' ';
         tree.insert(x);
     }
-//    tree.print();
+    std::cout << std::endl;
+    tree.print();
+    std::cout << std::endl;
+    auto it = tree.begin();
+    for(int i=0;i<6;i++){
+        std::cout << *it << ' ';
+        ++it;
+    }
+    for(int i=0;i<6;i++){
+        std::cout << *it << ' ';
+        --it;
+    }
+    std::cout << *it;
+    std::cout << std::endl;
+    tree.print();
+    std::cout << std::endl;
     std::vector b = a;
 
     std::shuffle(b.begin(), b.end(), gen);
-    for(auto x : b){
-        tree.erase(x);
+
+    for(int i=0;i<b.size()/2; ++i){
+        std::cout << b[i] << ' ';
+        tree.erase(b[i]);
+        tree.print();
+        std::cout << std::endl;
     }
+    std::cout << std::endl;
     tree.print();
 
 }
